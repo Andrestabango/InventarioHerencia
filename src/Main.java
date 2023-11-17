6769cabfc12deab9982428fb4f1a08c92ea7159a
Andrestabango <andres.tabango@udla.edu.ec>
2023-11-17 08:55:58 -0500

    Herencia01

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Producto p1 = new Producto();
+        p1.menuPrincipal();
 
-        Alimento a1=new Alimento();
-        a1.ingresarProducto();
-        a1.imprimirProducto();
 
-        Electrodomestico e1=new Electrodomestico();
-        e1.ingresarProducto();
-        e1.imprimirProducto();
-
-        Tecnologia t1=new Tecnologia();
-        t1.ingresarProducto();
-        t1.imprimirProducto();
 
 
     }
--- src/Producto.java
+++ src/Producto.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Producto {
@@ -5,7 +6,7 @@ public class Producto {
     String nombre;
     double precio;
     String codigo;
-    int cantidad;
+    int cantidad,opcion;
     
     String marca;
 
@@ -39,8 +40,39 @@ public class Producto {
         System.out.println("Marca del producto: "+marca);
     }
 
+public void menuPrincipal(){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("MENU DE PRODUCTOS");
+        System.out.println("1.ALIMENTOS");
+        System.out.println("2.ELECTRODOMESTICOS");
+        System.out.println("3.TECNOLOGIA");
+        System.out.println("Ingrese el numero de la accion que desea hacer:");
+        this.opcion=sc.nextInt();
+        if(opcion==1){
+            System.out.println("ALIMENTOS");
+            Alimento a1=new Alimento();
+            a1.ingresarProducto();
+            a1.imprimirProducto();
+        }
+        else if(opcion==2){
+            System.out.println("ELECTRODOMESTICOS");
+            Electrodomestico e1=new Electrodomestico();
+            e1.ingresarProducto();
+            e1.imprimirProducto();
+            }
+        else if(opcion==3){
+            System.out.println("TECNOLOGIA");
+            Tecnologia t1=new Tecnologia();
+            t1.ingresarProducto();
+            t1.imprimirProducto();
+            }
+        }
+
+
+
+    }
 
 
 
 
-}
